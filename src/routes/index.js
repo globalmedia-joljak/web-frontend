dec95c023300700a58b719448829a21cfc9a6220
gay0ung <6and_end9@naver.com>
2021-02-23 18:21:14 +0900

    update HeaderPage

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as Projects } from "./Projects";
+export { default as Tbuild } from "./Tbuild";
+export { default as Notice } from "./Notice";
+export { default as Search } from "./Search";
+export { default as MyPage } from "./MyPage";
+export { default as NotFound } from "./NotFound";
