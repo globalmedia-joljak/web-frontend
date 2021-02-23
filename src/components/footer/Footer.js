79c59d70fc3083347cb3d35b56636740284cd982
gay0ung <6and_end9@naver.com>
2021-02-23 18:18:34 +0900

    create FooterPage

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./footerStyle.scss";
+const Footer = () => {
+  return (
+    <footer>
+      <div className="inr">
+        <span className="e-mail">G-Mail: moses011010@gmail.com</span>
+        <div className="f-logo"></div>
+        <span className="copy">
+          Copyright 2021. 최강남대문쌍절곤 All right reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
