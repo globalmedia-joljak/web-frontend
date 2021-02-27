48d2ae0fb33a02b8fd2a50f7eef70b2109a165c9
gay0ung <6and_end9@naver.com>
2021-02-27 20:42:13 +0900

    update fix_confilct
    _mixin.scss 오류 수정.

--- src/components/main/home/HomePage.js
+++ src/components/main/home/HomePage.js
@@ -1,23 +1,24 @@
 import React from "react";
 import Footer from "../../footer/Footer.js";
-import "../mainStyle.scss";
+import "./homeStyle.scss";
+
 const HomePage = () => {
   return (
-    <div className="home">
+    <div className="home-wrap">
       <div className="heroImg"></div>
-      <section className="">
-        <h2>글로벌미디어학부 학생들을 위한 '졸작' 서비스</h2>
-        <p>
-          쉽게 졸업작품 프로젝트를 위한 팀을 매칭시켜주고, 역대 졸업 작품들을
-          관람할 수 있습니다.
-        </p>
-        <div className="home-video">
-          <video controls height="100%">
-            <source src="" type="IMG/MOV" />
-          </video>
-          <h3>2021 글로벌미디어학부 졸업작품 '묻고 답하다' 스케치</h3>
-        </div>
-      </section>
+      <div className="section-wrap">
+        <section>
+          <h2>글로벌미디어학부 학생들을 위한 '졸작' 서비스</h2>
+          <p>
+            쉽게 졸업작품 프로젝트를 위한 팀을 매칭시켜주고, 역대 졸업 작품들을
+            관람할 수 있습니다.
+          </p>
+          <div className="home-video">
+            <video controls>{/* <source src="" type="IMG/MOV" /> */}</video>
+            <h3>2021 글로벌미디어학부 졸업작품 '묻고 답하다' 스케치</h3>
+          </div>
+        </section>
+      </div>
       <Footer />
     </div>
   );
