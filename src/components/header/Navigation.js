39f355e6e682f3253d6ab19bf8eeebae88fba568
hamryt <rkdl0829@naver.com>
2021-02-28 19:55:31 +0900

    signup에 header 안보이게 적용

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -7,225 +7,225 @@ import "./headerStyle.scss";
 
 // routes간격
 const NavStyling = (navEl, size, tablet) => {
-  const [hStyle, setHstyle] = useState({});
+	const [hStyle, setHstyle] = useState({});
 
-  useEffect(() => {
-    const { parentElement } = navEl.current;
-    if (size > tablet) {
-      const parentWidth = parentElement.clientWidth,
-        calculated = Math.floor(parentWidth * 0.07),
-        maxGap = 96;
+	useEffect(() => {
+		const { parentElement } = navEl.current;
+		if (size > tablet) {
+			const parentWidth = parentElement.clientWidth,
+				calculated = Math.floor(parentWidth * 0.07),
+				maxGap = 96;
 
-      calculated > maxGap
-        ? setHstyle({ gap: `${maxGap}px` })
-        : setHstyle({ gap: `${calculated}px` });
-    } else {
-      return setHstyle({ gap: 0 });
-    }
-  }, [navEl, setHstyle, size, tablet]);
+			calculated > maxGap
+				? setHstyle({ gap: `${maxGap}px` })
+				: setHstyle({ gap: `${calculated}px` });
+		} else {
+			return setHstyle({ gap: 0 });
+		}
+	}, [navEl, setHstyle, size, tablet]);
 
-  return { hStyle };
+	return { hStyle };
 };
 
 //로딩후 넓이와 resize될때 넓이값에 따라 nav에 clickEvent를 준다.
 const SizeChecking = (curSize, navEl, pathname) => {
-  const [size, setSize] = useState(curSize);
+	const [size, setSize] = useState(curSize);
 
-  const handleResize = () => setSize(window.innerWidth);
+	const handleResize = () => setSize(window.innerWidth);
 
-  useEffect(() => {
-    if (pathname === "/signin" || pathname === "/signout") return;
+	useEffect(() => {
+		if (pathname === "/signin" || pathname === "/signup") return;
 
-    let { style } = navEl.current.offsetParent.nextSibling;
+		let { style } = navEl.current.offsetParent.nextSibling;
 
-    window.addEventListener("resize", handleResize);
-    style.display = navEl.current.classList.contains("on") ? "none" : "block";
+		window.addEventListener("resize", handleResize);
+		style.display = navEl.current.classList.contains("on") ? "none" : "block";
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	});
 
-  return { size };
+	return { size };
 };
 
 const NavClickEvent = (size, tablet, navEl) => {
-  useEffect(() => {
-    if (navEl.current === undefined) return;
+	useEffect(() => {
+		if (navEl.current === undefined) return;
 
-    const handleClick = (e) => {
-      let { classList, parentElement, id } = e.target;
-      let { style } = navEl.current.offsetParent.nextSibling;
+		const handleClick = (e) => {
+			let { classList, parentElement, id } = e.target;
+			let { style } = navEl.current.offsetParent.nextSibling;
 
-      const ON = "on";
+			const ON = "on";
 
-      const tagFilter = (className) => {
-        return classList.contains(className) ?? false;
-      };
+			const tagFilter = (className) => {
+				return classList.contains(className) ?? false;
+			};
 
-      switch (true) {
-        case tagFilter("h-nav"):
-          !tagFilter(ON) ? classList.add(ON) : classList.remove(ON);
+			switch (true) {
+				case tagFilter("h-nav"):
+					!tagFilter(ON) ? classList.add(ON) : classList.remove(ON);
 
-          style.display = navEl.current.classList.contains(ON)
-            ? "none"
-            : "block";
-          return;
+					style.display = navEl.current.classList.contains(ON)
+						? "none"
+						: "block";
+					return;
 
-        // routes일 경우
-        case tagFilter("header-route"):
-          const SHOW = "show";
+				// routes일 경우
+				case tagFilter("header-route"):
+					const SHOW = "show";
 
-          if (id === "teams" || id === "project") {
-            return parentElement.classList.contains(SHOW)
-              ? parentElement.classList.remove(SHOW)
-              : parentElement.classList.add(SHOW);
-          }
+					if (id === "teams" || id === "project") {
+						return parentElement.classList.contains(SHOW)
+							? parentElement.classList.remove(SHOW)
+							: parentElement.classList.add(SHOW);
+					}
 
-          navEl.current.classList.remove(ON);
-          break;
+					navEl.current.classList.remove(ON);
+					break;
 
-        default:
-          navEl.current.classList.remove(ON);
-          style.display = "block";
-          break;
-      }
-    };
+				default:
+					navEl.current.classList.remove(ON);
+					style.display = "block";
+					break;
+			}
+		};
 
-    // tablet사이즈일때 부터
-    if (size < tablet) {
-      navEl.current.addEventListener("click", handleClick);
-    } else if (size > tablet) {
-      navEl.current.classList.remove("on");
-      navEl.current.removeEventListener("click", handleClick);
-    }
+		// tablet사이즈일때 부터
+		if (size < tablet) {
+			navEl.current.addEventListener("click", handleClick);
+		} else if (size > tablet) {
+			navEl.current.classList.remove("on");
+			navEl.current.removeEventListener("click", handleClick);
+		}
 
-    return () => {
-      navEl.current.removeEventListener("click", handleClick);
-    };
-  }, [size, tablet, navEl]);
+		return () => {
+			navEl.current.removeEventListener("click", handleClick);
+		};
+	}, [size, tablet, navEl]);
 
-  return { navEl };
+	return { navEl };
 };
 
 // tablet ~ phone / sub_nav_template
 const SubNavigation = (id) => {
-  if (id === null) return;
+	if (id === null) return;
 
-  const { teamList, projectList } = subRoutes;
+	const { teamList, projectList } = subRoutes;
 
-  let data = [];
-  switch (id) {
-    case "teams":
-      data = teamList;
-      break;
+	let data = [];
+	switch (id) {
+		case "teams":
+			data = teamList;
+			break;
 
-    case "project":
-      data = projectList;
-      break;
+		case "project":
+			data = projectList;
+			break;
 
-    default:
-      break;
-  }
+		default:
+			break;
+	}
 
-  return data.map(({ path, name, id }, i) => {
-    return (
-      <li key={i}>
-        {
-          <Link to={path} className="header-route" id={id}>
-            {name}
-          </Link>
-        }
-      </li>
-    );
-  });
+	return data.map(({ path, name, id }, i) => {
+		return (
+			<li key={i}>
+				{
+					<Link to={path} className="header-route" id={id}>
+						{name}
+					</Link>
+				}
+			</li>
+		);
+	});
 };
 
 // routes_template
 const RouterLink = (size, tablet) => {
-  let data = "";
+	let data = "";
 
-  let linkData = navRoutes;
-  let webNav = linkData.filter((_, i) => i < linkData.length - 1);
+	let linkData = navRoutes;
+	let webNav = linkData.filter((_, i) => i < linkData.length - 1);
 
-  size > tablet ? (data = webNav) : (data = linkData);
+	size > tablet ? (data = webNav) : (data = linkData);
 
-  return data.map(({ path, name, id }) => {
-    return (
-      <li key={id}>
-        <Link to={path} className="header-route" id={id}>
-          {name}
-          {(id === "teams" || id === "project") && size < tablet && (
-            <div id="more-icon">
-              <span></span>
-              <span className="rotate"></span>
-            </div>
-          )}
-        </Link>
-        {(id === "teams" || id === "project") && size < tablet && (
-          <ul className="nav-sub-wrap">{SubNavigation(id)}</ul>
-        )}
-      </li>
-    );
-  });
+	return data.map(({ path, name, id }) => {
+		return (
+			<li key={id}>
+				<Link to={path} className="header-route" id={id}>
+					{name}
+					{(id === "teams" || id === "project") && size < tablet && (
+						<div id="more-icon">
+							<span></span>
+							<span className="rotate"></span>
+						</div>
+					)}
+				</Link>
+				{(id === "teams" || id === "project") && size < tablet && (
+					<ul className="nav-sub-wrap">{SubNavigation(id)}</ul>
+				)}
+			</li>
+		);
+	});
 };
 
 const Navigation = ({ location: { pathname } }) => {
-  // if (pathname === "/signin" || pathname === "/signup") return;
-  const navEl = useRef();
+	// if (pathname === "/signin" || pathname === "/signup") return;
+	const navEl = useRef();
 
-  // 현재 사이즈 확인.
-  const tablet = 768;
-  const { size } = SizeChecking(window.innerWidth, navEl, pathname);
+	// 현재 사이즈 확인.
+	const tablet = 768;
+	const { size } = SizeChecking(window.innerWidth, navEl, pathname);
 
-  const { hStyle } = NavStyling(navEl, size, tablet);
+	const { hStyle } = NavStyling(navEl, size, tablet);
 
-  // roter lists
-  NavClickEvent(size, tablet, navEl);
-  const router = RouterLink(size, tablet);
+	// roter lists
+	NavClickEvent(size, tablet, navEl);
+	const router = RouterLink(size, tablet);
 
-  const hideHeader = () => {
-    return pathname === "/signin" || pathname === "/signout" ? "none" : "block";
-  };
+	const hideHeader = () => {
+		return pathname === "/signin" || pathname === "/signup" ? "none" : "block";
+	};
 
-  return (
-    <header
-      className={pathname === "/" ? "light" : "dark"}
-      style={{ display: `${hideHeader()}` }}
-    >
-      <div className="header-inr inr">
-        <div className="header-inr-wrap" style={{ ...hStyle }}>
-          <h1 className="header-logo">
-            <Link to="/" />
-          </h1>
+	return (
+		<header
+			className={pathname === "/" ? "light" : "dark"}
+			style={{ display: `${hideHeader()}` }}
+		>
+			<div className="header-inr inr">
+				<div className="header-inr-wrap" style={{ ...hStyle }}>
+					<h1 className="header-logo">
+						<Link to="/" />
+					</h1>
 
-          <nav className="h-nav" ref={navEl}>
-            {size < tablet ? (
-              <div className="header-nav-tablet">
-                <div className="header-nav-tablet-inr">
-                  <h1 className="header-logo">
-                    <Link to="/" className="header-route" />
-                  </h1>
-                  <ul className="nav-inr-wrap">{router}</ul>
-                </div>
-              </div>
-            ) : (
-              <ul className="nav-inr-wrap" style={{ ...hStyle }}>
-                {router}
-              </ul>
-            )}
-          </nav>
-        </div>
+					<nav className="h-nav" ref={navEl}>
+						{size < tablet ? (
+							<div className="header-nav-tablet">
+								<div className="header-nav-tablet-inr">
+									<h1 className="header-logo">
+										<Link to="/" className="header-route" />
+									</h1>
+									<ul className="nav-inr-wrap">{router}</ul>
+								</div>
+							</div>
+						) : (
+							<ul className="nav-inr-wrap" style={{ ...hStyle }}>
+								{router}
+							</ul>
+						)}
+					</nav>
+				</div>
 
-        <div className="mypage">
-          <button className="mypage-btn">
-            <Link to="/mypage">user</Link>
-            <span>님, 안녕하세요</span>
-          </button>
-        </div>
-      </div>
-    </header>
-  );
+				<div className="mypage">
+					<button className="mypage-btn">
+						<Link to="/mypage">user</Link>
+						<span>님, 안녕하세요</span>
+					</button>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default withRouter(Navigation);
