4b167a967b6a8ef803260bf93eed7cf2118fc54d
gay0ung <6and_end9@naver.com>
2021-03-09 23:09:05 +0900

    Update MyPage.js

--- src/routes/MyPage.js
+++ src/routes/MyPage.js
@@ -1,12 +1,8 @@
 import React from "react";
-import MyPageForm from "../components/main/mypage/MyPageForm.js";
+// import MyPageForm from "../components/main/mypage/MyPageForm.js";
 
 const MyPage = () => {
-  return (
-    <main>
-      <MyPageForm />
-    </main>
-  );
+  return <main>{/* <MyPageForm /> */}</main>;
 };
 
 export default MyPage;
