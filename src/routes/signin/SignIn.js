af1bfbc2dc5de9b9dd8f475c37f51da7a321ea89
hamryt <rkdl0829@naver.com>
2021-03-14 16:50:56 +0900

    클래스에서 함수형으로 변경

--- src/routes/signin/SignIn.js
+++ src/routes/signin/SignIn.js
@@ -1,76 +1,68 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Signinbackground from "./image/SigninbackgroundHalf.png";
 import GlobalMediaLogo from "./image/GlobalMedia_Logo.png";
 import style from "./SignIn.scss";
 
-class SignIn extends PureComponent {
-	constructor(props) {
-		super(props);
-
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<div className="login-container">
-				<div className="login-left login">
-					<div className="GlobalMedia-logo">
-						<img
-							src={GlobalMediaLogo}
-							alt="My dongho"
-							className="GlobalMediaLogo"
-						/>
-					</div>
-					<div className="login-form-div">
-						<form className="login-form">
-							<input
-								className="login-input"
-								type="text"
-								name="query"
-								placeholder="학번"
-							/>
-							<div className="check-info"></div>
-							<input
-								className="login-input"
-								type="password"
-								name="query"
-								placeholder="비밀번호"
-							/>
-							<div className="check-info"></div>
-
-							<label className="remember-id-container">
-								아이디 기억하기
-								<input type="checkbox" />
-								<span className="remember-id-checkmark"></span>
-							</label>
-
-							<button className="log-button" type="submit">
-								로그인
-							</button>
-						</form>
-					</div>
-					<p className="url-signUp">
-						<a href="/signup" className="signUp">
-							회원가입
-						</a>
-					</p>
-				</div>
-
-				<div className="login-right login">
+const SignIn = () => {
+	return (
+		<div className="login-container">
+			<div className="login-left login">
+				<div className="GlobalMedia-logo">
 					<img
-						src={Signinbackground}
+						src={GlobalMediaLogo}
 						alt="My dongho"
-						className="Signinbackground"
+						className="GlobalMediaLogo"
 					/>
-					<p className="url-signUp">
-						<a href="/signup" className="signUp">
-							회원가입
-						</a>
-					</p>
 				</div>
+				<div className="login-form-div">
+					<form className="login-form">
+						<input
+							className="login-input"
+							type="text"
+							name="query"
+							placeholder="학번"
+						/>
+						<div className="check-info"></div>
+						<input
+							className="login-input"
+							type="password"
+							name="query"
+							placeholder="비밀번호"
+						/>
+						<div className="check-info"></div>
+
+						<label className="remember-id-container">
+							아이디 기억하기
+							<input type="checkbox" />
+							<span className="remember-id-checkmark"></span>
+						</label>
+
+						<button className="log-button" type="submit">
+							로그인
+						</button>
+					</form>
+				</div>
+				<p className="url-signUp">
+					<a href="/signup" className="signUp">
+						회원가입
+					</a>
+				</p>
 			</div>
-		);
-	}
-}
+
+			<div className="login-right login">
+				<img
+					src={Signinbackground}
+					alt="My dongho"
+					className="Signinbackground"
+				/>
+				<p className="url-signUp">
+					<a href="/signup" className="signUp">
+						회원가입
+					</a>
+				</p>
+			</div>
+		</div>
+	);
+};
 
 export default SignIn;
--- src/routes/signup/SignUp.js
+++ src/routes/signup/SignUp.js
@@ -1,173 +1,173 @@
-import React, { PureComponent } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import style from "./SignUp.scss";
 
-class SignUp extends PureComponent {
-	constructor(props) {
-		super(props);
+const SignUp = (props) => {
+	// constructor(props) {
+	// 	super(props);
 
-		this.state = {
-			input: {},
-			errors: {},
-		};
+	// 	this.state = {
+	// 		input: {},
+	// 		errors: {},
+	// 	};
 
-		this.handleChange = this.handleChange.bind(this);
-	}
+	// 	this.handleChange = this.handleChange.bind(this);
+	// }
 
-	handleChange(event) {
-		let input = this.state.input;
-		let errors = {};
-		input[event.target.name] = event.target.value;
+	const [passwd, setPasswd] = useState("");
+	const [confirmPasswd, setConfirmPasswd] = useState("");
+	const [error, setError] = useState("");
 
-		if (
-			typeof input["password"] !== "undefined" &&
-			typeof input["confirm_password"] !== "undefined"
-		) {
-			if (input["password"] != input["confirm_password"]) {
-				errors["password"] = "비밀번호가 일치하지 않습니다.";
-			}
-		}
+	useEffect(() => {
+		passwd === confirmPasswd
+			? setError("")
+			: setError("비밀번호가 일치하지 않습니다.");
+	});
 
-		this.setState({
-			input,
-			errors: errors,
-		});
-	}
+	const handleChange = (event) => {
+		const { value } = event.target;
 
-	goToHome() {
-		this.props.history.push(`/`);
-	}
+		setPasswd(value);
+		// name === "confirm_password" ?? setConfirmPasswd(value);
 
-	render() {
-		return (
-			<div className="signUp-container">
-				<div className="el-main">
-					<form onSubmit={this.handleSubmit}>
-						<div className="sub-info">
-							<div className="info-title">계정정보</div>
+		// if (passwd !== "") {
+		// 	passwd !== confirmPasswd
+		// 		? setError("비밀번호가 일치하지 않습니다.")
+		// 		: setError("");
+		// }
+		// if (
+		// 	typeof input["password"] !== "undefined" &&
+		// 	typeof input["confirm_password"] !== "undefined"
+		// ) {
+		// 	if (passwd != input["confirm_password"]) {
+		// 		errors["password"] = "비밀번호가 일치하지 않습니다.";
+		// 	}
+		// }
 
-							<div className="info-body">
-								<div className="input-id group-start">
-									<input
-										type="text"
-										name="student_code"
-										id="student_code"
-										value={this.state.input.student_code}
-										placeholder="학번을 입력해주세요"
-										onChange={this.handleChange}
-										className="signUp-input"
-									/>
-									<div className="check-info">
-										{this.state.errors.student_code}
-									</div>
-								</div>
+		// this.setState({
+		// 	input,
+		// 	errors: errors,
+		// });
+	};
 
-								<div className="input-password">
-									<div className="form1-input-password">
-										<input
-											type="password"
-											name="password"
-											id="password"
-											value={this.state.input.password}
-											placeholder="비밀번호를 입력하세요"
-											onChange={this.handleChange}
-											className="signUp-input passwrod1"
-										/>
-										<div className="check-info">
-											{this.state.errors.password}
-										</div>
-									</div>
+	const goToHome = () => {
+		props.history.push(`/`);
+	};
 
-									<div className="form2-input-password ">
-										<input
-											type="password"
-											name="confirm_password"
-											id="confirm_password"
-											value={this.state.input.confirm_password}
-											placeholder="비밀번호를 다시 확인하세요"
-											onChange={this.handleChange}
-											className="signUp-input"
-										/>
-										<div className="check-info">
-											{this.state.errors.confirm_password}
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+	return (
+		<div className="signUp-container">
+			<div className="el-main">
+				<form>
+					<div className="sub-info">
+						<div className="info-title">계정정보</div>
 
-						<div className="sub-info">
-							<div className="info-title">개인정보</div>
-
-							<div className="info-body">
-								<div className="input-name group-start">
-									<input
-										type="text"
-										name="name"
-										id="name"
-										value={this.state.input.name}
-										placeholder="이름을 입력해주세요"
-										onChange={this.handleChange}
-										className="signUp-input insert-name"
-									/>
-
-									<select className="select-job">
-										<option disabled="disabled" value>
-											직군
-										</option>
-										<option value="designer">디자이너</option>
-										<option value="dev">개발자</option>
-										<option value="planner">기획자</option>
-									</select>
-								</div>
+						<div className="info-body">
+							<div className="input-id group-start">
+								<input
+									type="text"
+									name="student_code"
+									id="student_code"
+									placeholder="학번을 입력해주세요"
+									onChange={handleChange}
+									className="signUp-input"
+								/>
 								<div className="check-info"></div>
+							</div>
 
-								<div className="input-phone">
+							<div className="input-password">
+								<div className="form1-input-password">
 									<input
-										type="text"
-										name="phone_number"
-										id="phone_number"
-										value={this.state.input.phone_number}
-										placeholder="핸드폰 번호를 입력하세요"
-										onChange={this.handleChange}
+										type="password"
+										name="password"
+										id="password"
+										value={passwd}
+										placeholder="비밀번호를 입력하세요"
+										onChange={(e) => setPasswd(e.target.value)}
+										className="signUp-input passwrod1"
+									/>
+									<div className="check-info">{error}</div>
+								</div>
+
+								<div className="form2-input-password ">
+									<input
+										type="password"
+										name="confirm_password"
+										id="confirm_password"
+										value={confirmPasswd}
+										placeholder="비밀번호를 다시 확인하세요"
+										onChange={(e) => setConfirmPasswd(e.target.value)}
 										className="signUp-input"
 									/>
-								</div>
-								<div className="check-info">
-									{this.state.errors.phone_number}
+									<div className="check-info"></div>
 								</div>
 							</div>
 						</div>
+					</div>
 
-						<div className="sub-info">
-							<div className="info-title">인증번호</div>
+					<div className="sub-info">
+						<div className="info-title">개인정보</div>
 
-							<div className="info-body">
-								<div className="input-auth-num group-start">
-									<input
-										type="text"
-										name="auth_number"
-										id="auth_number"
-										value={this.state.input.auth_number}
-										placeholder="인증번호를 입력하세요"
-										onChange={this.handleChange}
-										className="signUp-input"
-									/>
-								</div>
-								<div className="check-info">
-									{this.state.errors.auth_number}
-								</div>
+						<div className="info-body">
+							<div className="input-name group-start">
+								<input
+									type="text"
+									name="name"
+									id="name"
+									placeholder="이름을 입력해주세요"
+									onChange={() => handleChange}
+									className="signUp-input insert-name"
+								/>
+
+								<select className="select-job">
+									<option disabled="disabled" value>
+										직군
+									</option>
+									<option value="designer">디자이너</option>
+									<option value="dev">개발자</option>
+									<option value="planner">기획자</option>
+								</select>
 							</div>
-						</div>
+							<div className="check-info"></div>
 
-						<button onClick={() => this.goToHome()} className="signUp-button">
-							가입하기
-						</button>
-					</form>
-				</div>
+							<div className="input-phone">
+								<input
+									type="text"
+									name="phone_number"
+									id="phone_number"
+									placeholder="핸드폰 번호를 입력하세요"
+									onChange={() => handleChange}
+									className="signUp-input"
+								/>
+							</div>
+							<div className="check-info"></div>
+						</div>
+					</div>
+
+					<div className="sub-info">
+						<div className="info-title">인증번호</div>
+
+						<div className="info-body">
+							<div className="input-auth-num group-start">
+								<input
+									type="text"
+									name="auth_number"
+									id="auth_number"
+									placeholder="인증번호를 입력하세요"
+									onChange={() => handleChange}
+									className="signUp-input"
+								/>
+							</div>
+							<div className="check-info"></div>
+						</div>
+					</div>
+
+					<button onClick={() => this.goToHome()} className="signUp-button">
+						가입하기
+					</button>
+				</form>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default SignUp;
