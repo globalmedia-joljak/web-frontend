9ac13d80ae9abe3f352ee837600439245b39e3bc
gay0ung <58800923+gay0ung@users.noreply.github.com>
2021-03-16 14:52:14 +0900

    Feature/create signup modal (#23)
    * create signup_popup_modal
    
    * Update MyPage.js
    
    * update signupPage-popup

--- src/routes/MyPage.js
+++ src/routes/MyPage.js
@@ -2,7 +2,11 @@ import React from "react";
 // import MyPageForm from "../components/main/mypage/MyPageForm.js";
 
 const MyPage = () => {
-  return <main>{/* <MyPageForm /> */}</main>;
+  return (
+    <main>
+      {/* <MyPageForm /> */}
+    </main>
+  );
 };
 
 export default MyPage;
