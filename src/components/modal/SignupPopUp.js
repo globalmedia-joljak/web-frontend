b924c3f73f7ae4ddd3d2ab997d700ef80c810d89
gay0ung <6and_end9@naver.com>
2021-03-16 15:56:53 +0900

    update header
    git 되돌리기를 잘못해서 다시 아까 보냈던 pr다시 보냅니다....

--- src/components/modal/SignupPopUp.js
+++ src/components/modal/SignupPopUp.js
@@ -1,87 +1,88 @@
-import React, { useReducer, useState } from "react";
-import "./popUpStyle.scss";
-
-const PopUpEvents = (name) => {
-  const reducer = (state, { value, name }) => {
-    return {
-      ...state,
-      [name]: value,
-    };
-  };
-  const [choiceValue, FETCH_OCCUPATION] = useReducer(reducer, {
-    Representative: "",
-    Additional: "",
-  });
-
-  const HandleChange = (e) => FETCH_OCCUPATION(e.target);
-  console.log(choiceValue);
-  return { HandleChange };
-};
-
-const lists = (occupations, name) => {
-  const className = name === "Representative" ? "repre" : "add";
-  const { HandleChange } = PopUpEvents(name);
-
-  return occupations.map((data, i, arr) => (
-    <li key={i}>
-      <input
-        id={className + i}
-        type="radio"
-        name={name}
-        value={data}
-        onChange={(e) => HandleChange(e)}
-      />
-      <label htmlFor={className + i}>{data}</label>
-    </li>
-  ));
-};
+import React, {
+  useCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from 'react';
+import './popUpStyle.scss';
+import OccupationListForm from './OccupationListForm.js';
 
 const SignUpPopUp = () => {
-  const [modalView, setModalView] = useState(true);
-  const occupations = ["미디어아트", "디자이너", "개발자", "기획자"];
-  const handleSubmit = (e) => {};
-  const handleCancel = (e) => {
-    console.dir(e.target);
-    setModalView(true);
+  const ulRefs = useRef();
+  const [modalView, setModalView] = useState(false);
+  const [role, setRolse] = useState({
+    mainProjectRole: '',
+    subProjectRole: '',
+  });
+
+  const [disabled, setDisabled] = useState(false);
+  const { mainProjectRole, subProjectRole } = role;
+
+  useEffect(() => {
+    if (mainProjectRole === '' && subProjectRole === '') return;
+    if (mainProjectRole === subProjectRole) setDisabled(true);
+  });
+
+  const handleChoice = (e) => {
+    const { value, name, form } = e.target;
+
+    setRolse({
+      ...role,
+      [name]: value,
+    });
+
+    const checkLists = Array.from(form).map((inputEl) => {
+      if (inputEl.nodeName !== 'INPUT') return;
+
+      inputEl.disabled = false;
+    });
   };
-  const modalStyle = modalView ? "none" : "grid";
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: 직군 선택 한후 데이터 PETCH해줘야 한다.
+    console.log('전송됐다');
+  };
+  const handleCancel = (e) => setModalView(true);
+  const modalStyle = modalView ? 'none' : 'grid';
+
   return (
     <div className="modal-wrapper" style={{ display: modalStyle }}>
-      <div className="modal-inr">
+      <form className="modal-inr" onSubmit={handleSubmit}>
         <ul className="modal-lists">
-          <li className={`list r2 `}>
+          <li className={`list r2`}>
             <h2>직군설정</h2>
             <p>
               자신의 직군을 설정하세요.
-              <span>(대표 1개, 부가직군3개까지 설정 가능)</span>
+              <span>(대표 1개, 부가직군1개까지 설정 가능)</span>
             </p>
           </li>
           <li className="list r2">
             <h3>대표직군</h3>
-            <ul>{lists(occupations, "Representative")}</ul>
+            <OccupationListForm
+              type="mainProjectRole"
+              handleChoice={handleChoice}
+            />
           </li>
           <li className="list r2">
             <h3>부가직군</h3>
-            <ul>{lists(occupations, "Additional")}</ul>
+            <OccupationListForm
+              type="subProjectRole"
+              handleChoice={handleChoice}
+              ulRefs={ulRefs}
+            />
           </li>
           <li className="list alert">
-            <p>회원가입 후 마이페이지에서 변경할 수 있습니다.</p>
+            <p>회원가입 후 작가등록 페이지에서 변경할 수 있습니다.</p>
           </li>
           <li className="list choice-btn">
-            <button
-              type="submit"
-              onClick={(e) => {
-                handleSubmit(e);
-              }}
-            >
-              선택완료
-            </button>
+            <button type="submit">선택완료</button>
           </li>
           <li id="cancel-btn">
             <button type="button" onClick={handleCancel}></button>
           </li>
         </ul>
-      </div>
+      </form>
     </div>
   );
 };
