c20a20343e91ea15f1e2d968cabe375a5e46d142
hodong <51500359+namdongho@users.noreply.github.com>
2021-03-17 13:42:27 +0900

    change select to modal (#29)
    * change select to modal
    
    * 코드스타일 수정
    
    * parameter 추가
    
    Co-authored-by: hamryt <rkdl0829@naver.com>

--- src/routes/signup/SignUp.js
+++ src/routes/signup/SignUp.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import SignUpPopUp from '../../components/modal/SignupPopUp';
+import selectJobIcon from './image/Modal@2x.png';
 import style from './SignUp.scss';
 
 const SignUp = (props) => {
   const [passwd, setPasswd] = useState('');
   const [confirmPasswd, setConfirmPasswd] = useState('');
   const [error, setError] = useState('');
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     passwd === confirmPasswd
@@ -15,128 +18,125 @@ const SignUp = (props) => {
 
   const handleChange = (event) => {
     const { value } = event.target;
-
-    setPasswd(value);
+    console.log(event.target);
   };
 
-  const goToHome = () => {
+  const handleSignUpSubmit = () => {
     props.history.push(`/`);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShow(!show);
   };
 
   return (
     <div className="signUp-container">
-      <div className="el-main">
-        <form>
-          <div className="sub-info">
-            <div className="info-title">계정정보</div>
-
-            <div className="info-body">
-              <div className="input-id group-start">
-                <input
-                  type="text"
-                  name="student_code"
-                  id="student_code"
-                  placeholder="학번을 입력해주세요"
-                  onChange={() => handleChange}
-                  className="signUp-input"
-                />
-                <div className="check-info"></div>
-              </div>
-
-              <div className="input-password">
-                <div className="form1-input-password">
+      {show ? (
+        <SignUpPopUp show={show} setShow={setShow} />
+      ) : (
+        <div className="el-main">
+          <form>
+            <div className="sub-info">
+              <div className="info-title">계정정보</div>
+              <div className="info-body">
+                <div className="input-id group-start">
                   <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={passwd}
-                    placeholder="비밀번호를 입력하세요"
-                    onChange={(e) => setPasswd(e.target.value)}
-                    className="signUp-input passwrod1"
-                  />
-                  <div className="check-info">{error}</div>
-                </div>
-
-                <div className="form2-input-password ">
-                  <input
-                    type="password"
-                    name="confirm_password"
-                    id="confirm_password"
-                    value={confirmPasswd}
-                    placeholder="비밀번호를 다시 확인하세요"
-                    onChange={(e) => setConfirmPasswd(e.target.value)}
+                    type="text"
+                    name="student_code"
+                    id="student_code"
+                    placeholder="학번을 입력해주세요"
+                    onChange={(e) => handleChange(e)}
                     className="signUp-input"
                   />
                   <div className="check-info"></div>
                 </div>
+                <div className="input-password">
+                  <div className="form1-input-password">
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      value={passwd}
+                      placeholder="비밀번호를 입력하세요"
+                      onChange={(e) => setPasswd(e.target.value)}
+                      className="signUp-input passwrod1"
+                    />
+                    <div className="check-info">{error}</div>
+                  </div>
+                  <div className="form2-input-password ">
+                    <input
+                      type="password"
+                      name="confirm_password"
+                      id="confirm_password"
+                      value={confirmPasswd}
+                      placeholder="비밀번호를 다시 확인하세요"
+                      onChange={(e) => setConfirmPasswd(e.target.value)}
+                      className="signUp-input"
+                    />
+                    <div className="check-info"></div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="sub-info">
-            <div className="info-title">개인정보</div>
-
-            <div className="info-body">
-              <div className="input-name group-start">
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="이름을 입력해주세요"
-                  onChange={() => handleChange}
-                  className="signUp-input insert-name"
-                />
-
-                <select className="select-job">
-                  <option disabled="disabled" value>
-                    직군
-                  </option>
-                  <option value="designer">디자이너</option>
-                  <option value="dev">개발자</option>
-                  <option value="planner">기획자</option>
-                </select>
+            <div className="sub-info">
+              <div className="info-title">개인정보</div>
+              <div className="info-body">
+                <div className="input-name group-start">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="이름을 입력해주세요"
+                    onChange={(e) => handleChange(e)}
+                    className="signUp-input insert-name"
+                  />
+                  <button className="select-job" onClick={handleClick}>
+                    직군 선택
+                    <img
+                      src={selectJobIcon}
+                      alt="No icon"
+                      className="selectJobIcon"
+                    />
+                  </button>
+                </div>
+                <div className="check-info"></div>
+                <div className="input-phone">
+                  <input
+                    type="text"
+                    name="phone_number"
+                    id="phone_number"
+                    placeholder="핸드폰 번호를 입력하세요"
+                    onChange={(e) => handleChange(e)}
+                    className="signUp-input"
+                  />
+                </div>
+                <div className="check-info"></div>
               </div>
-              <div className="check-info"></div>
-
-              <div className="input-phone">
-                <input
-                  type="text"
-                  name="phone_number"
-                  id="phone_number"
-                  placeholder="핸드폰 번호를 입력하세요"
-                  onChange={() => handleChange}
-                  className="signUp-input"
-                />
-              </div>
-              <div className="check-info"></div>
             </div>
-          </div>
-
-          <div className="sub-info">
-            <div className="info-title">인증번호</div>
-
-            <div className="info-body">
-              <div className="input-auth-num group-start">
-                <input
-                  type="text"
-                  name="auth_number"
-                  id="auth_number"
-                  placeholder="인증번호를 입력하세요"
-                  onChange={() => handleChange}
-                  className="signUp-input"
-                />
+            <div className="sub-info">
+              <div className="info-title">인증번호</div>
+              <div className="info-body">
+                <div className="input-auth-num group-start">
+                  <input
+                    type="text"
+                    name="auth_number"
+                    id="auth_number"
+                    placeholder="인증번호를 입력하세요"
+                    onChange={(e) => handleChange(e)}
+                    className="signUp-input"
+                  />
+                </div>
+                <div className="check-info"></div>
               </div>
-              <div className="check-info"></div>
             </div>
-          </div>
-
-          <button onClick={() => this.goToHome()} className="signUp-button">
-            가입하기
-          </button>
-        </form>
-      </div>
+            <button onClick={handleSignUpSubmit} className="signUp-button">
+              가입하기
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
-
 export default SignUp;
