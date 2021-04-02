926b69e9cb37d91eb061191fdf997b082e8beaa6
gay0ung <6and_end9@naver.com>
2021-04-02 17:54:32 +0900

    Update users.js
    정보 수정 실패했을경우 알림창 추가

--- src/service/api/users.js
+++ src/service/api/users.js
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import { client } from './client';
 
 //유저 정보;
@@ -8,11 +9,14 @@ const GetUser = async (classOf) => {
 
 const updateUserInfo = async (classOf, url, value) => {
   try {
-    return await client.patch(`users/${classOf}/${url}`, value, {
+    await client.patch(`users/${classOf}/${url}`, value, {
       headers: { 'Content-Type': `application/json` },
     });
+    return;
   } catch (e) {
-    console.log(e);
+    toast.error(
+      '⛔추가연락처 설정이 실패하였습니다. 입력하신 정보가 맞는지 확인해 주세요.',
+    );
   }
 };
 
