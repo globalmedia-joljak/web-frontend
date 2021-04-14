792c1710b7b4f50ab2330b1b8bd36059ea56e9b5
MinSeong <cms3136@gmail.com>
2021-04-14 09:51:22 +0900

    팀 생성하기 UI 구현 (#48)
    * create_team_ui
    
    * apply review

--- src/components/main/teamBuild/team/index.js
+++ src/components/main/teamBuild/team/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router';
 import TeamList from './teamList/TeamList';
+import CreateTeam from './createTeam/CreateTeam';
 
 const Teams = ({ match }) => {
   return (
     <>
       <Route exact path={`${match.path}`} component={TeamList} />
-      {/* <Route path={`${match.path}/:id`} component={AuthorDetail} /> */}
+      <Route path={`${match.path}/create`} component={CreateTeam} />
     </>
   );
 };
