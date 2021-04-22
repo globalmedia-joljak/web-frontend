3b3a861e4c60710ae4196421f4e7e86ca03bb8ae
gay0ung <58800923+gay0ung@users.noreply.github.com>
2021-04-22 14:40:09 +0900

    회의 때 이슈 수정 (#58)

--- src/components/main/teamBuild/author/authorList/ListOfAuthorForm.js
+++ src/components/main/teamBuild/author/authorList/ListOfAuthorForm.js
@@ -1,67 +1,82 @@
 import React, { useCallback, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppState } from '../../../../../context/appContext';
-import {
-  useTeamsDispatch,
-  useTeamsState,
-} from '../../../../../context/teamContext';
+import { useTeamsDispatch } from '../../../../../context/teamContext';
 import useAsync from '../../../../../hooks/useAsync';
 import ModalTemp from '../../../../modal/ModalTemp';
 import OccupationListForm from '../../../../modal/OccupationListForm';
 import ThereIsNoList from '../../ThereIsNoList';
 import './authorStyle.scss';
 import { getAuthorProfileList } from '../../../../../service/api/profile.js';
-import CreateAuthor from '../createAuthor/CreateAuthor';
+
 import { toast, ToastContainer } from 'react-toastify';
 import ButtonWIthIcon from '../../../common/ButtonWIthIcon.js';
+import { useEffect } from 'react/cjs/react.development';
 
 const ListOfAuthorForm = ({ match, history }) => {
   const {
+    infinite,
     userInfo: { isLogin, classOf },
   } = useAppState();
   const { translationKR, setJobColor } = useAppDispatch();
-  const { filterClassOf, setShowCreate, setDefaultImg } = useTeamsDispatch();
-  const { showCreate } = useTeamsState();
+  const { filterClassOf, setDefaultImg } = useTeamsDispatch();
 
   const createAuthorEl = useRef();
 
   const [filterShow, setFilterShow] = useState(false);
-  const [filterRole, setFilterRole] = useState('');
+  const [filterRole, setFilterRole] = useState(null);
 
   const [profilePageNum, setProfilePageNum] = useState(0);
+  const [authorList, setAuthorList] = useState([]);
+  const [filterList, setFilterList] = useState([]);
+  const [totalPage, setTotalPage] = useState(0);
 
-  const [profileList] = useAsync(() => getAuthorProfileList(profilePageNum), [
-    profilePageNum,
-  ]);
-
-  // TODO : 스크롤시 데이터를 추가로 불러와 주어야 한다(setProfilePageNum)
-  const { loading, data, error } = profileList;
+  const [profileList, refetch] = useAsync(
+    () => getAuthorProfileList(profilePageNum),
+    [profilePageNum],
+  );
 
   const handleFilter = useCallback((e) => setFilterShow(!filterShow));
   const handleChoice = useCallback((e) => setFilterRole(e.target.value), [
     setFilterRole,
   ]);
 
-  const handleSubmit = (e) => setFilterShow(!filterShow);
+  const [total, setTotal] = useState(true);
 
+  useEffect(() => {
+    // TODO : 스크롤시 데이터를 추가로 불러와 주어야 한다
+    if (!profileList.data) return;
+    setAuthorList(profileList.data.content);
+
+    const { scrollTop, scrollHeight, clientHeight } = infinite;
+    if (scrollTop === 0 || clientHeight === 0 || scrollHeight === 0) return;
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+      console.log('여기서 데이터를 더 가져와야 한다.');
+    }
+  }, [profileList, profilePageNum, totalPage, infinite]);
+
+  useEffect(() => {
+    if (!filterRole || filterRole === '전체') {
+      setFilterList(authorList);
+    } else {
+      setFilterList(
+        authorList.filter((author) => {
+          return filterRole === author.user.mainProjectRole;
+        }),
+      );
+    }
+  }, [authorList, filterRole, setFilterList]);
+
+  const { loading, data, error } = profileList;
   if (loading) return <div>로딩중...</div>;
   if (!data) return null;
   if (error) return <div></div>;
 
-  let authorListData = [];
-
-  if (filterRole === '' || filterRole === '전체') {
-    authorListData = data.content;
-  } else {
-    authorListData = data.content.filter(
-      (author) => author.user.mainProjectRole === filterRole,
-    );
-  }
+  const handleSubmit = (e) => setFilterShow(!filterShow);
 
   const addAuthorHandler = (e) => {
-    console.log(classOf, isLogin);
     if (!isLogin) {
-      setShowCreate(false);
       toast.error(
         '⛔ 로그인했을 경우 작가등록이 가능합니다. 로그인 페이지로 이동합니다.',
       );
@@ -78,10 +93,12 @@ const ListOfAuthorForm = ({ match, history }) => {
       const message =
         '이미 작가등록을 하셨습니다. 작가 목록 페이지로 가시겠습니까?';
 
-      if (window.confirm(message)) history.push(`/author/${classOf}`);
+      if (window.confirm(message)) history.push(`${match.url}/${classOf}`);
+
+      return false;
     }
 
-    setShowCreate(true);
+    history.push(`${match.url}/${classOf}/create`);
   };
 
   return (
@@ -120,86 +137,76 @@ const ListOfAuthorForm = ({ match, history }) => {
       ) : (
         <>
           <div className="hero-img"></div>
-          <div className="author-wrap" ref={createAuthorEl}>
-            {showCreate ? (
-              <CreateAuthor type="create" history={history} />
-            ) : (
-              <>
-                <h3>
-                  전체 작가 인원<span>{authorListData.length}</span>
-                </h3>
+          <div className="author-wrap content-size" ref={createAuthorEl}>
+            <h3>
+              전체 작가 인원
+              <span>{total ? data.totalElements : filterList.length}</span>
+            </h3>
 
-                {authorListData.length < 1 ? (
-                  <ThereIsNoList />
-                ) : (
-                  <ul className="author-list">
-                    {authorListData.map(
-                      ({
-                        id,
-                        user: {
-                          classOf,
-                          name,
-                          mainProjectRole,
-                          subProjectRole,
-                        },
-                        mediaInfo,
-                      }) => (
-                        <li key={id}>
-                          <Link to={`${match.path}/${classOf}`}>
-                            <b className="main-role">
-                              {translationKR(mainProjectRole)}
-                              <span
-                                className="main-role-hover"
-                                style={{
-                                  backgroundColor: setJobColor(mainProjectRole),
-                                }}
-                              ></span>
-                            </b>
-                            <strong className="author-name">
-                              {name}
-                              <span>{filterClassOf(classOf)}학번</span>
-                            </strong>
-                            <span
-                              className="sub-role"
-                              style={{
-                                borderColor: setJobColor(subProjectRole),
-                                color: setJobColor(subProjectRole),
-                              }}
-                            >
-                              {translationKR(subProjectRole)}
-                            </span>
-                            <i
-                              className="author-img"
-                              style={{
-                                backgroundImage: `url(${
-                                  mediaInfo
-                                    ? mediaInfo.url
-                                    : setDefaultImg(mainProjectRole)
-                                })`,
-                              }}
-                            >
-                              <i className="hover-bg" />
-                            </i>
-                          </Link>
-                        </li>
-                      ),
-                    )}
-                  </ul>
+            {filterList.length < 1 ? (
+              <ThereIsNoList />
+            ) : (
+              <ul className="author-list">
+                {filterList.map(
+                  ({
+                    id,
+                    user: { classOf, name, mainProjectRole, subProjectRole },
+                    mediaInfo,
+                  }) => (
+                    <li key={id}>
+                      <Link to={`${match.path}/${classOf}`}>
+                        <b className="main-role">
+                          {translationKR(mainProjectRole)}
+                          <span
+                            className="main-role-hover"
+                            style={{
+                              backgroundColor: setJobColor(mainProjectRole),
+                            }}
+                          ></span>
+                        </b>
+                        <strong className="author-name">
+                          {name}
+                          <span>{filterClassOf(classOf)}학번</span>
+                        </strong>
+                        <span
+                          className="sub-role"
+                          style={{
+                            borderColor: setJobColor(subProjectRole),
+                            color: setJobColor(subProjectRole),
+                          }}
+                        >
+                          {translationKR(subProjectRole)}
+                        </span>
+                        <i
+                          className="author-img"
+                          style={{
+                            backgroundImage: `url(${
+                              mediaInfo
+                                ? mediaInfo.url
+                                : setDefaultImg(mainProjectRole)
+                            })`,
+                          }}
+                        >
+                          <i className="hover-bg" />
+                        </i>
+                      </Link>
+                    </li>
+                  ),
                 )}
-                <div className="author-functions">
-                  <ButtonWIthIcon
-                    btntype="filter"
-                    btnTxt="상세검색"
-                    handleButton={handleFilter}
-                  />
-                  <ButtonWIthIcon
-                    btntype="create"
-                    btnTxt="등록하기"
-                    handleButton={addAuthorHandler}
-                  />
-                </div>
-              </>
+              </ul>
             )}
+            <div className="author-functions">
+              <ButtonWIthIcon
+                btntype="filter"
+                btnTxt="상세검색"
+                handleButton={handleFilter}
+              />
+              <ButtonWIthIcon
+                btntype="create"
+                btnTxt="등록하기"
+                handleButton={addAuthorHandler}
+              />
+            </div>
           </div>
         </>
       )}
--- src/components/main/teamBuild/author/index.js
+++ src/components/main/teamBuild/author/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router';
+import { Route } from 'react-router';
 import AuthorDetail from './authorList/AuthorDetail.js';
 import ListOfAuthorForm from './authorList/ListOfAuthorForm.js';
 import CreateAuthor from './createAuthor/CreateAuthor.js';
@@ -9,8 +9,7 @@ const AuthorForm = ({ match }) => {
     <>
       <Route exact path={`${match.path}`} component={ListOfAuthorForm} />
       <Route exact path={`${match.path}/:id`} component={AuthorDetail} />
-      {/* <Route path={`${match.path}/:id/:state`} component={CreateAuthor} /> */}
-      {/* <Redirect /> */}
+      <Route exact path={`${match.path}/:id/:state`} component={CreateAuthor} />
     </>
   );
 };
